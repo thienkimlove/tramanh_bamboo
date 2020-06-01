498bc07bb4ef939e39c9abb1af948d4cdff90b29
QuanDo <thienkimlove@gmail.com>
2020-06-01 16:27:49 +0700

    update

---
+++ resources/views/frontend/landing.blade.php
@@ -0,0 +1,213 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>LANDING</title>
+    <link rel="stylesheet" href="/frontend/css/bootstrap-3.3.2.min.css">
+    <link rel="stylesheet" href="/frontend/css/owl.carousel.css">
+    <link rel="stylesheet" href="/frontend/css/landing.css">
+</head>
+<body>
+<div class="wrapper">
+    <a href="tel:0983058689" class="btn_hotline" title="0983 05 86 89" aria-label="Tel"><span class="icon"><svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" preserveAspectRatio="none" viewBox="0 0 1408 1896.0833" fill="rgba(0, 0, 0, 1.0)"> <path d="M1408 1240q0 27-10 70.5t-21 68.5q-21 50-122 106-94 51-186 51-27 0-52.5-3.5T959 1520t-47.5-14.5T856 1485t-49-18q-98-35-175-83-128-79-264.5-215.5T152 904q-48-77-83-175-3-9-18-49t-20.5-55.5T16 577 3.5 519.5 0 467q0-92 51-186 56-101 106-122 25-11 68.5-21t70.5-10q14 0 21 3 18 6 53 76 11 19 30 54t35 63.5 31 53.5q3 4 17.5 25t21.5 35.5 7 28.5q0 20-28.5 50t-62 55-62 53-28.5 46q0 9 5 22.5t8.5 20.5 14 24 11.5 19q76 137 174 235t235 174q2 1 19 11.5t24 14 20.5 8.5 22.5 5q18 0 46-28.5t53-62 55-62 50-28.5q14 0 28.5 7t35.5 21.5 25 17.5q25 15 53.5 31t63.5 35 54 30q70 35 76 53 3 7 3 21z"></path> </svg></span>0983 05 86 89</a>
+    <div id="section1" style="background: url('https://img.youtube.com/vi/Sm5Jlui9nLE/maxresdefault.jpg') center top no-repeat;">
+        <video playsinline poster='https://img.youtube.com/vi/Sm5Jlui9nLE/maxresdefault.jpg' oncontextmenu="return false;" muted="" preload="auto" loop="loop" class="lazyload" id="videoCover" autoplay="autoplay"><source src="/frontend/images/video.mp4" type="video/mp4"></video>
+        <a href="javascript:void(0)" class="btn_on_off_speaker" aria-label="Bật tắt tiếng"></a>
+    </div>
+    <div id="section2" class="section lazyload">
+        <div class="text">
+            <p class="txtTop">Quảng cáo trên tạp chí (Print-ads, PR artical, Collaboration, ..)</p>
+            <p class="txtMiddle">Like magazine <br>
+                Unlike any magazine</p>
+            <p class="txtBottom">A LINK between YOUR BRAND & CUSTOMERS</p>
+        </div>
+        <div class="img">
+            <a href="javascript:void(0)" class="bg" style="background: url('/frontend/images/1.jpg') center center no-repeat;" title="Like magazine Unlike any magazine" aria-label="Like magazine Unlike any magazine">Like magazine Unlike any magazine</a>
+        </div>
+    </div>
+    <div id="section3" class="section lazyload">
+        <div class="img">
+            <a href="javascript:void(0)" class="bg" style="background: url('/frontend/images/2.jpg') center center no-repeat;" title="The Best for The Unique" aria-label="The Best for The Unique">The Best for The Unique</a>
+        </div>
+        <div class="text txtRight">
+            <p class="txtTop">Quảng cáo trên vé máy bay</p>
+            <p class="txtMiddle">The Best for The Unique</p>
+            <p class="txtBottom">Điều tuyệt vời nhất cho vị trí độc tôn</p>
+        </div>
+    </div>
+    <div id="section4" class="section lazyload">
+        <div class="text">
+            <p class="txtTop">Quảng cáo trên thân tàu bay</p>
+            <p class="txtMiddle">Imposing display. Impresive appearance</p>
+            <p class="txtBottom">Hiển thị hoành tráng. Xuất hiện ấn tượng</p></p>
+        </div>
+        <div class="img">
+            <a href="javascript:void(0)" class="bg" style="background: url('/frontend/images/3.jpg') center center no-repeat;" title="Imposing display. Impresive appearance" aria-label="Imposing display. Impresive appearance">Imposing display. Impresive appearance</a>
+        </div>
+    </div>
+    <div id="section5" class="section lazyload">
+        <div class="img">
+            <a href="javascript:void(0)" class="bg" style="background: url(/frontend/images/4.jpg) center 40% no-repeat;" title="Beside head. Inside mind" aria-label="Beside head. Inside mind">Beside head. Inside mind</a>
+        </div>
+        <div class="text txtRight">
+            <p class="txtTop">Quảng cáo tựa đầu ghế máy bay <br>
+            </p>
+            <p class="txtMiddle">
+                Beside head. Inside mind
+            </p>
+        </div>
+    </div>
+    <div id="section6" class="section lazyload">
+        <div class="text">
+            <p class="txtTop">Quảng cáo TVC trên máy bay</p>
+            <p class="txtMiddle">Lively run to customers’ mind</p>
+            <p class="txtBottom">Chạy thẳng vào tiềm thức khách hàng</p>
+        </div>
+        <div class="img">
+            <a href="javascript:void(0)" class="bg" style="background: url('/frontend/images/5.jpg') center bottom no-repeat;" title="Lively run to customers’ mind" aria-label="Lively run to customers’ mind">Lively run to customers’ mind</a>
+        </div>
+    </div>
+    <div id="section7" class="section lazyload">
+        <div class="img scrollFirst">
+            <a href="javascript:void(0)" class="bg" style="background: url('/frontend/images/6.jpg') center bottom no-repeat;" aria-label="Quảng cáo trên hộp đựng đồ ăn">Quảng cáo trên hộp đựng đồ ăn</a>
+            <div class="txt">
+                <span>Quảng cáo trên hộp đựng đồ ăn</span>
+            </div>
+        </div>
+        <div class="img scrollFirst">
+            <a href="javascript:void(0)" class="bg" style="background: url('/frontend/images/7.jpg') center center no-repeat;" aria-label="Quảng cáo trên giấy lót khay ăn">Quảng cáo trên giấy lót khay ăn</a>
+            <div class="txt">
+                <span>Quảng cáo trên giấy lót khay ăn</span>
+            </div>
+        </div>
+        <div class="img scrollSecond">
+            <a href="javascript:void(0)" class="bg" style="background: url('/frontend/images/8.jpg') center bottom no-repeat;" aria-label="Quảng cáo trên khoang hành lý">Quảng cáo trên khoang hành lý</a>
+            <div class="txt">
+                <span>Quảng cáo trên khoang hành lý</span>
+            </div>
+        </div>
+        <div class="img scrollSecond">
+            <a href="javascript:void(0)" class="bg" style="background: url('/frontend/images/9.jpg') center bottom no-repeat;" aria-label="Quảng cáo trên IFE">Quảng cáo trên IFE</a>
+            <div class="txt">
+                <span>Quảng cáo trên IFE</span>
+            </div>
+        </div>
+    </div>
+
+    <div id="section8" class="section lazyload">
+        <h2 class="title"><span>OUR VALUED CUSTOMERS</span> | <span>Thương hiệu đã tham gia</span></h2>
+        <div id="slider1">
+            <img data-src="/frontend/images/logo/1-boeing.jpg" alt="Boeing" class="lazyload">
+            <img data-src="/frontend/images/logo/2-mercedes.jpg" alt="Mercedes" class="lazyload">
+            <img data-src="/frontend/images/logo/3-Hublot.jpg" alt="Hublot" class="lazyload">
+            <img data-src="/frontend/images/logo/4-Chopard.jpg" alt="Chopard" class="lazyload">
+            <img data-src="/frontend/images/logo/5-porsche.jpg" alt="Porsche" class="lazyload">
+            <img data-src="/frontend/images/logo/6-samsung.jpg" alt="Samsung" class="lazyload">
+            <img data-src="/frontend/images/logo/7-Korloff.jpg" alt="Korloff" class="lazyload">
+            <img data-src="/frontend/images/logo/7-SHBbank.jpg" alt="SHB Bank" class="lazyload">
+            <img data-src="/frontend/images/logo/8-honda_logo.jpg" alt="Honda" class="lazyload">
+            <img data-src="/frontend/images/logo/9-anh-khue.jpg" alt="Anh Khuê Sài Gòn" class="lazyload">
+            <img data-src="/frontend/images/logo/9-casio-edifice.jpg" alt="Edifice Casio" class="lazyload">
+            <img data-src="/frontend/images/logo/9-IC.jpg" alt="IC" class="lazyload">
+            <img data-src="/frontend/images/logo/10-coach.jpg" alt="Coach" class="lazyload">
+            <img data-src="/frontend/images/logo/10-pre.jpg" alt="Premier Village" class="lazyload">
+            <img data-src="/frontend/images/logo/11-loc-phuc.jpg" alt="Lộc Phúc" class="lazyload">
+            <img data-src="/frontend/images/logo/11-pull-man.jpg" alt="Pull Man" class="lazyload">
+            <img data-src="/frontend/images/logo/11wyndham.jpg" alt="Wyndham" class="lazyload">
+            <img data-src="/frontend/images/logo/12-vpbank.jpg" alt="VP Bank" class="lazyload">
+            <img data-src="/frontend/images/logo/13-the-shells.jpg" alt="The Shells" class="lazyload">
+            <img data-src="/frontend/images/logo/14-doji.jpg" alt="Doji" class="lazyload">
+            <img data-src="/frontend/images/logo/15-vmg.jpg" alt="Valentino Creations" class="lazyload">
+            <img data-src="/frontend/images/logo/16-sealife.jpg" alt="Sea Life" class="lazyload">
+            <img data-src="/frontend/images/logo/17-inoac.jpg" alt="Inoac" class="lazyload">
+            <img data-src="/frontend/images/logo/18-orient.jpg" alt="Orient" class="lazyload">
+            <img data-src="/frontend/images/logo/19-OCB.jpg" alt="OCB Bank" class="lazyload">
+            <img data-src="/frontend/images/logo/19-panasonic.jpg" alt="Panasonic" class="lazyload">
+            <img data-src="/frontend/images/logo/20-Rimowa.jpg" alt="Rimowa" class="lazyload">
+            <img data-src="/frontend/images/logo/20-vietyacht.jpg" alt="Viety Acht" class="lazyload">
+            <img data-src="/frontend/images/logo/21-caravelle.jpg" alt="Caravelle" class="lazyload">
+            <img data-src="/frontend/images/logo/22-paradise.jpg" alt="Paradise" class="lazyload">
+            <img data-src="/frontend/images/logo/23-rex-hotel.jpg" alt="Rex Hotel" class="lazyload">
+            <img data-src="/frontend/images/logo/24-banana.jpg" alt="Bannana Republic" class="lazyload">
+            <img data-src="/frontend/images/logo/24-tms.jpg" alt="TMS Group" class="lazyload">
+            <img data-src="/frontend/images/logo/25-dolce.jpg" alt="Dolce" class="lazyload">
+            <img data-src="/frontend/images/logo/26-casta.jpg" alt="Casta" class="lazyload">
+            <img data-src="/frontend/images/logo/27-vinfast.jpg" alt="Vinfast" class="lazyload">
+            <img data-src="/frontend/images/logo/28-flamingo.jpg" alt="Flamingo Đại Lải Resort" class="lazyload">
+            <img data-src="/frontend/images/logo/29-rita-vo.jpg" alt="Rita Giảng Võ" class="lazyload">
+            <img data-src="/frontend/images/logo/30-mat-quoc-te-dnd.jpg" alt="Bệnh viện mắt quốc tế DND" class="lazyload">
+            <img data-src="/frontend/images/logo/31-autogrill.jpg" alt="Auto Grill" class="lazyload">
+            <img data-src="/frontend/images/logo/32-heritage.jpg" alt="Heritage Cruises" class="lazyload">
+            <img data-src="/frontend/images/logo/33-hitachi.jpg" alt="Hitachi" class="lazyload">
+            <img data-src="/frontend/images/logo/34-ford.jpg" alt="Ford" class="lazyload">
+            <img data-src="/frontend/images/logo/35-Audi-Logo.jpg" alt="Audi" class="lazyload">
+            <img data-src="/frontend/images/logo/36-ovs.jpg" alt="OVS Italian Fashion" class="lazyload">
+            <img data-src="/frontend/images/logo/37-bidv.jpg" alt="BIDV Bank" class="lazyload">
+            <img data-src="/frontend/images/logo/38-bwp.jpg" alt="BWP Premier" class="lazyload">
+            <img data-src="/frontend/images/logo/40-bo.jpg" alt="Bo Bang and Olufsen" class="lazyload">
+            <img data-src="/frontend/images/logo/41-ryoma-golf.jpg" alt="Ryoma Golf" class="lazyload">
+        </div>
+    </div>
+    <div id="section9" class="section lazyload">
+        <h2 class="title">Reviews</h2>
+        <div id="slider2">
+            <img data-src="/frontend/images/review/1.jpg" alt="Review 1" class="lazyload">
+            <img data-src="/frontend/images/review/2.jpg" alt="Review 2" class="lazyload">
+            <img data-src="/frontend/images/review/3a.jpg" alt="Review 3" class="lazyload">
+            <img data-src="/frontend/images/review/3b.jpg" alt="Review 4" class="lazyload">
+            <img data-src="/frontend/images/review/4.jpg" alt="Review 5" class="lazyload">
+            <img data-src="/frontend/images/review/5.jpg" alt="Review 6" class="lazyload">
+            <img data-src="/frontend/images/review/6.jpg" alt="Review 7" class="lazyload">
+            <img data-src="/frontend/images/review/7.jpg" alt="Review 8" class="lazyload">
+            <img data-src="/frontend/images/review/8.jpg" alt="Review 9" class="lazyload">
+            <img data-src="/frontend/images/review/8b.jpg" alt="Review 10" class="lazyload">
+            <img data-src="/frontend/images/review/9.jpg" alt="Review 11" class="lazyload">
+            <img data-src="/frontend/images/review/10.jpg" alt="Review 12" class="lazyload">
+            <img data-src="/frontend/images/review/11.jpg" alt="Review 13" class="lazyload">
+            <img data-src="/frontend/images/review/12.jpg" alt="Review 14" class="lazyload">
+            <img data-src="/frontend/images/review/13.jpg" alt="Review 15" class="lazyload">
+            <img data-src="/frontend/images/review/14.jpg" alt="Review 16" class="lazyload">
+            <img data-src="/frontend/images/review/15.jpg" alt="Review 17" class="lazyload">
+            <img data-src="/frontend/images/review/16.jpg" alt="Review 18" class="lazyload">
+            <img data-src="/frontend/images/review/17.jpg" alt="Review 19" class="lazyload">
+            <img data-src="/frontend/images/review/18.jpg" alt="Review 20" class="lazyload">
+            <img data-src="/frontend/images/review/19.jpg" alt="Review 21" class="lazyload">
+            <img data-src="/frontend/images/review/20.jpg" alt="Review 22" class="lazyload">
+        </div>
+    </div>
+    <footer>
+        <p>HOTLINE: <a href="tel:0983058689" title="Tel" aria-label="Tel">0983 05 86 89</a></p>
+        <p>Email: <a href="mailTo:quangcaobambooairways.vn@gmail.com" title="Email" aria-label="Email">quangcaobambooairways.vn@gmail.com</a></p>
+        <p>Youtube: <a href="https://www.youtube.com/channel/UCpmIe3_KU-SE9MWhcb9ZZmg" target="_blank" rel="noreferrer" aria-label="Youtube Chanel" title="Youtube Chanel">quangcaobambooairways</a></p>
+    </footer>
+    <div class="popup px" id="popup2">
+        <div class="popup-content pa">
+            <a href="javascript:void(0)" class="btn_close pa" title="Đóng lại" aria-label="Đóng lại">x</a>
+            <div class="bg"></div>
+            <div class="right">
+                <div><div style="text-transform: uppercase;font-weight: bold;">Vui lòng scan để</div> <span>quangcaobambooairways.vn</span> gửi báo giá và chiết khấu cao nhất dành cho chuyên gia MKT</div>
+                <div class="qrcode">
+                    <img data-src="/frontend/images/qrcode.jpg" alt="Qrcode" class="imgFull lazyload">
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="popup px" id="popup1">
+        <div class="popup-content pa">
+            <div class="bg"></div>
+            <div class="right">
+                <h3>Thông tin dành riêng cho chuyên gia Marketing</h3>
+                <p>Hãy xác nhận bằng cách bấm vào</p>
+                <a href="javascript:void(0)" id="btnContinue" title="Tiếp tục" aria-label="Tiếp tục">Tiếp tục</a>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
+<script type="text/javascript" src="/frontend/js/jquery-1.11.1.min.js"></script>
+<!--<script type="text/javascript" src="/frontend/js/SmoothScroll.js"></script>-->
+<script type="text/javascript" src="/frontend/js/lazysizes.min.js"></script>
+<script type="text/javascript" src="/frontend/js/owl.carousel.js"></script>
+<script type="text/javascript" src="/frontend/js/landing.js"></script>
